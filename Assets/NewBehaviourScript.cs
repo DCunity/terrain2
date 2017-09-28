5e8a7b44e7360932b979d29549ce3e4a18289c31
DCunity <32368631+DCunity@users.noreply.github.com>
2017-09-28 19:12:28 +0200

    test
    test

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int zmienna1 = 4;
+        int zmienna2 = 3;
+        int zmienna3 = 2 * (zmienna1 + zmienna2) - 5;
+        print(zmienna3); //wyświetli ?
+        print(3 * zmienna3); //wyświetli ?
+        print(zmienna3); //wyświetli ?
+  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
